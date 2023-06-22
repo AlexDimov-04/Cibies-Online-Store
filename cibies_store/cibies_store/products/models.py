8fb39822e86f150a2e4e136b4b57134a25a9fc4b
Alexander Dimov <alex.lemuel@gmail.com>
2023-06-22 17:15:32 +0300

    added price column

--- cibies_store/cibies_store/products/models.py
+++ cibies_store/cibies_store/products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from cibies_store.user_profile.models import Profile
 from cibies_store.user_profile.validators import alphanumeric_charcters_validator
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MinValueValidator
 
 class Product(models.Model):
     MAX_LENGTH_PRODUCT_NAME = 30
+    MIN_VALUE_PRICE = 1
+    MIN_VALUE_WEIGHT = 0.1
 
     name = models.CharField(
         max_length=MAX_LENGTH_PRODUCT_NAME,
@@ -15,7 +16,13 @@ class Product(models.Model):
 
     description = models.TextField()
 
-    weight = models.IntegerField(
+    price = models.FloatField(
+        validators=[MinValueValidator(MIN_VALUE_PRICE)],
+        default=''
+    )
+
+    weight = models.FloatField(
         null=True,
         blank=True,
+        validators=[MinValueValidator(MIN_VALUE_WEIGHT)]
     )
